dd143e3902d94164b45929149a5acb5213f0f994
GitLobanov <lobanovgithub@gmail.com>
2024-10-11 16:45:05 +0700

    Init project

---
+++ consciforavocab/src/main/java/com/conscifora/vocab/ConsciforavocabApplication.java
@@ -0,0 +1,13 @@
+package com.conscifora.vocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsciforavocabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsciforavocabApplication.class, args);
+    }
+
+}
